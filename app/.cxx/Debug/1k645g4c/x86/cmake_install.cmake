f48d005aa891208198ef539915a5e2151a36ece0
MarcelinLeP <131677976+MarcelinLeP@users.noreply.github.com>
2023-09-05 16:10:25 +0200

    avancer 1

--- app/.cxx/Debug/1k645g4c/x86/cmake_install.cmake
+++ app/.cxx/Debug/1k645g4c/x86/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/puipp/Downloads/Kart/Kart/app/src/main/cpp
+# Install script for directory: C:/Users/puipp/StudioProjects/Kart/app/src/main/cpp
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/puipp/Downloads/Kart/Kart/app/.cxx/Debug/1k645g4c/x86/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/puipp/StudioProjects/Kart/app/.cxx/Debug/1k645g4c/x86/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- app/src/main/java/ch/hevs/ss1/MyKartRemote.java
+++ app/src/main/java/ch/hevs/ss1/MyKartRemote.java
@@ -1,12 +1,18 @@
 package ch.hevs.ss1;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.ProgressBar;
 
 import android.util.Log;
 import android.widget.SeekBar;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import java.util.Timer;
 
 import ch.hevs.kart.AbstractKartControlActivity;
 import ch.hevs.kart.Kart;
@@ -22,25 +28,79 @@ public class MyKartRemote extends AbstractKartControlActivity {
         }
     };
     */
+
     TextView tv_battery;
+    TextView tv_position;
+    TextView tv_vitesse;
+    TextView tv_distance;
+    TextView tv_hall_cunt;
     SeekBar seekBar_vitesse;
-    SeekBar seekBar_position;
+    SeekBar sb_position;
+    ProgressBar pb_batterie;
+    Runnable runnable;
+
+    int counter;
+    Handler handler;
+    private View view;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kart_remote);
+        tv_battery = findViewById(R.id.tv_battery);
+        tv_position = findViewById(R.id.tv_position);
+        tv_vitesse = findViewById(R.id.tv_vitesse);
+        pb_batterie = findViewById(R.id.pb_batterie);
+        tv_distance = findViewById(R.id.tv_distance);
+        tv_hall_cunt = findViewById(R.id.tv_hall_cunt);
+        handler = new Handler();
 
-        //tv_battery = findViewById(R.id.tv_battery);
+
+        // Créez un Runnable pour gérer la mise à jour du compteur
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                // Incrémente le compteur
+                counter++;
+
+                // Répète l'exécution toutes les 100 ms
+                vitesse_change(view);
+                handler.postDelayed(this, 100);
+            }
+        };
+        handler.postDelayed(runnable, 100); // Démarre le compteur
 
         kart.addKartListener(new KartListener() {
+
+            @SuppressLint("DefaultLocale")
             @Override
-            public void batteryLevelChanged(Kart kart, double level) {
-                updateBatteryLevelUI(level);
+            public void batteryLevelChanged(@NonNull Kart kart, double level) {
+                KartListener.super.batteryLevelChanged(kart, level);
+                //updateBatteryLevelUI(level);
+                tv_battery.setText(String.format("%.1f%%", level * 100));
+                pb_batterie.setProgress((int) (level * 100));
+
+            }
+            @SuppressLint("DefaultLocale")
+            @Override
+            public void hallSensorCountChanged(@NonNull Kart  kart, int index , int valeur){
+                KartListener.super.hallSensorCountChanged(kart, 1, valeur);
+                tv_vitesse.setText(valeur);
+
+            }
+
+            @SuppressLint("DefaultLocale")
+            @Override
+            public void ultrasonicRangerDistanceChanged(@NonNull Kart kart, double distance){
+                KartListener.super.ultrasonicRangerDistanceChanged(kart, distance);
+                tv_distance.setText(String.format("%.1f",distance*100));
+
+
             }
         });
 
-        // Référencez la SeekBar par son ID
+                // Référencez la SeekBar par son ID
         seekBar_vitesse = findViewById(R.id.seekBar_vitesse);
 
         // Ajoutez un écouteur de changement de valeur à la SeekBar
@@ -48,7 +108,6 @@ public class MyKartRemote extends AbstractKartControlActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 // lorsque la valeur de la SeekBar change
-
                 kart.setDriveSpeed(seekBar_vitesse.getProgress()-15);
             }
             @Override
@@ -62,35 +121,35 @@ public class MyKartRemote extends AbstractKartControlActivity {
             }
         });
 
-        seekBar_position = findViewById(R.id.seekBar_position);
+        sb_position = findViewById(R.id.sb_position);
         // Ajoutez un écouteur de changement de valeur à la SeekBar
-        seekBar_vitesse.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        //La postion va de -30 à 30° dans ce cas sb_postion va de 0 à 200 (0.3° par changement)
+        sb_position.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @SuppressLint({"DefaultLocale", "SetTextI18n"})
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 // lorsque la valeur de la SeekBar change
+                tv_position.setText("Position : "+ String.format("%.1f", (sb_position.getProgress()-sb_position.getMax()/2)*0.3) + "°");
+                kart.setSteeringTargetPositionNormalized((double)(sb_position.getProgress()-sb_position.getMax()/2)/100);
             }
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-
+                //position_start_change = sb_position.getProgress()-sb_position.getMax()/2;
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
             }
         });
 
-    }
 
-    private void updateBatteryLevelUI(double level) {
-        tv_battery.setText((int) (level*100));
     }
-
 
     public void Acc_onClick(View view) {
         int speed_param;
         speed_param = kart.getDriveSpeed() + 1;
         kart.setDriveSpeed(speed_param);
+        seekBar_vitesse.setProgress(speed_param+15);
     }
 
     public void freiner_onClick(View view){
@@ -103,6 +162,7 @@ public class MyKartRemote extends AbstractKartControlActivity {
         }
 
         kart.setDriveSpeed(speed_param);
+        seekBar_vitesse.setProgress(speed_param+15);
 
     }
     public void reculer_onClick(View view) {
@@ -112,37 +172,50 @@ public class MyKartRemote extends AbstractKartControlActivity {
             speed_param = -1;
         }
         kart.setDriveSpeed(speed_param);
+        seekBar_vitesse.setProgress(speed_param+15);
     }
-
-
-    public void getBatterie(View view){
-        ProgressBar pb_batterie = findViewById(R.id.pb_batterie);
-        float battery_pourcent;
-        //timer_batterie.schedulePeriodically(500);
-        battery_pourcent = (float) (kart.getBatteryLevel())*100;
-        // Utilisation de Log pour afficher un message dans le Logcat
-        Log.d("TAG_1", "Ceci est un message de débogage");
-        Log.d("TAG_2", String.valueOf(kart.getBatteryLevel()));
-        Log.d("TAG_3", String.valueOf(battery_pourcent));
-        Log.d("TAG_31", String.valueOf(kart.getBatteryVoltage()));
-        pb_batterie.setProgress(10);
-    }
-
     public void goLeft_onClick(View view){
-        int value_turn;
-        Log.d("TAG_4", String.valueOf(kart.getSteeringPosition()));
-        value_turn = kart.getSteeringPosition()+40; //40 pas = 3°
-        Log.d("TAG_5", String.valueOf(value_turn));
-        kart.turnSteeringTargetPositionBy(value_turn);
+        if(sb_position.getProgress() >= 20){
+            sb_position.setProgress(sb_position.getProgress()-20);
+        }else{
+            sb_position.setProgress(0);
+        }
     }
 
     public void goRight_onClick(View view){
-        int value_turn;
-        Log.d("TAG_6", String.valueOf(kart.getSteeringPosition()));
-        value_turn = kart.getSteeringPosition()-40;
-        Log.d("TAG_7", String.valueOf(value_turn));
-        kart.turnSteeringTargetPositionBy(value_turn);
+        if(sb_position.getProgress() <= (sb_position.getMax()-20)){
+            Log.d("TAG_6", String.valueOf(kart.getSteeringPosition()));
+            sb_position.setProgress(sb_position.getProgress()+20);
+        }else{
+            sb_position.setProgress(sb_position.getMax());
+
+        }
     }
+    public void goPos0(View view){
+        sb_position.setProgress(sb_position.getMax()/2);
+        //tv_hall_cunt.setText(kart.getHallSensorCount(1));
+    }
+
+    int memory_cunt;
+    double vitesse_kart;
+    @SuppressLint("DefaultLocale")
+    public void vitesse_change(View view) {
+        this.view = view;
+        int temps = 0;
+        Log.d("TAG_10", String.valueOf(kart.getHallSensorCount(1)));
+        if(memory_cunt < kart.getHallSensorCount(1) ){
+            temps = kart.getHallSensorCount(1) - memory_cunt;
+        }
+        if (temps != 0) {
+            vitesse_kart = (3.14159 / 2500) / (temps / 36000.0); // Utilisez 36000.0 pour une division en virgule flottante
+            // Mettez à jour l'affichage de la vitesse
+            tv_vitesse.setText(String.valueOf(vitesse_kart) + " km/h");
+        }
+        memory_cunt = kart.getHallSensorCount(1);
+        tv_vitesse.setText(String.format("%.1f",vitesse_kart));
+    }
+
+
 
 
 }
